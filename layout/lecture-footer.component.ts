3717cc24349a97ac88eb375b45cf152aa2f2bac9
NhuY <leobachduong14@gmail.com>
2023-03-15 21:49:25 +0700

    Angular 100day and user-UI

---
+++ layout/lecture-footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './lecture-footer.component.html',
+  styleUrls: ['./lecture-footer.component.css']
+})
+export class NavbarComponent {
+
+}
